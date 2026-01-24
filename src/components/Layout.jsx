7c73ed5d5cf1facc7f40c77ce30663e1c389c520
sene32304@gmail.com <sene32304@gmail.com>
2026-01-24 15:35:57 +0000

    add Sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "./Header";
-import Sidebar from "./sidebar";
+import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
